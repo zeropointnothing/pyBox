cbb360d962c67e1f3b8feab8ec46e8796bcc3707
ZeroPointNothing <116678675+ZeroPointNothing@users.noreply.github.com>
2022-12-11 16:56:32 +0000

    multiple changes - Added final newline to pyPgs - Created unfinished Python script for Guess the Number

---
+++ GtN.py
@@ -0,0 +1,13 @@
+"""
+Guess the number game.
+"""
+from time import sleep
+
+def gtn_game():
+    """
+    The game itself.
+    """
+    print('Welcome to the Guess the Number game!')
+    sleep(2)
+    print('I will choose a number between 1 and 20. Then, you try and guess it!')
+gtn_game()
